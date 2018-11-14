b748a3ba0d27b51955169e5b25d08fd7b2f42e37
lt14 <592737616@qq.com>
2018-11-14 15:35:58 +0800

    first commit

---
+++ src/main/java/com/net/CampusApplication.java
@@ -0,0 +1,12 @@
+package com.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusApplication.class, args);
+	}
+}
