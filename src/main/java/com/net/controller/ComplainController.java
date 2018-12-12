93ca23ff6dad47422fd38e18c6b980bfb8e35c78
congye6 <244053679@qq.com>
2018-12-12 21:33:28 +0800

    投诉

---
+++ src/main/java/com/net/controller/ComplainController.java
@@ -0,0 +1,39 @@
+package com.net.controller;
+
+import com.net.service.ComplainService;
+import com.net.vo.ComplainVO;
+import com.net.vo.ResponseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cong on 2018-12-12.
+ */
+@RestController
+public class ComplainController {
+
+    @Autowired
+    private ComplainService complainService;
+
+    /**
+     * 新建投诉
+     * @param complainVO
+     * @return
+     */
+    @RequestMapping(value = "/complain/add",method = RequestMethod.POST)
+    public ResponseVO addComplain(ComplainVO complainVO){
+        return complainService.addComplain(complainVO);
+    }
+
+    /**
+     * 查询投诉
+     * @return
+     */
+    @RequestMapping(value = "/complain/all",method = RequestMethod.GET)
+    public ResponseVO getAllComplains(){
+        return complainService.getAllComplains();
+    }
+
+}
--- src/main/java/com/net/mapper/ComplainMapper.java
+++ src/main/java/com/net/mapper/ComplainMapper.java
@@ -3,7 +3,12 @@ package com.net.mapper;
 import com.net.vo.ComplainVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ComplainMapper {
     int insertSelective(ComplainVO record);
+
+    List<ComplainVO> selectAllComplain();
+
 }
