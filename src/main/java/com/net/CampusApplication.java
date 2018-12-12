7b4b06ed6a170daca5560ef8e9104c4a11607aab
jacksenma <915096289@qq.com>
2018-12-12 10:39:37 +0800

    上传图片

--- src/main/java/com/net/CampusApplication.java
+++ src/main/java/com/net/CampusApplication.java
@@ -1,10 +1,25 @@
 package com.net;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class CampusApplication {
+public class CampusApplication extends WebMvcConfigurerAdapter {
+
+	@Value("${web.upload.path}")
+	private String uploadPath;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+		registry.addResourceHandler("/uploads/**").addResourceLocations(
+				"file:"+uploadPath);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampusApplication.class, args);
